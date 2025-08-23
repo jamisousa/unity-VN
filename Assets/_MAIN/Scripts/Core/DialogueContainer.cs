ea5c24701ba144f7ea9c3f7fa3b6474bfb2bc2b4
jamisousa <jamiledsousa@gmail.com>
2025-08-23 12:27:02 -0300

    feat: adding text architect and base dialog functionality

---
+++ Assets/_MAIN/Scripts/Core/DialogueContainer.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueContainer
+{
+    public GameObject root;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+}
