7f75cebf5dbbe9f9d41b3c37f006de894b545442
jamisousa <jamiledsousa@gmail.com>
2025-09-06 10:57:08 -0300

    feat: adding file readers and initial parsing test scripts

--- Assets/_MAIN/Scripts/Core/DialogueContainer.cs
+++ Assets/_MAIN/Scripts/Core/DialogueContainer.cs
@@ -1,11 +1,15 @@
 using TMPro;
 using UnityEngine;
 
-[System.Serializable]
-public class DialogueContainer
+namespace DIALOGUE
 {
-    public GameObject root;
-    public TextMeshProUGUI nameText;
-    public TextMeshProUGUI dialogueText;
+    [System.Serializable]
+    public class DialogueContainer
+    {
+        public GameObject root;
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI dialogueText;
+
+    }
 
 }
