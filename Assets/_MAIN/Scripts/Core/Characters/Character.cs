288918efcb36450c8f4f1e67db436553cd72ddc5
jamisousa <jamiledsousa@gmail.com>
2025-10-26 10:36:47 -0300

    feat: adding separate character dialogue system

--- Assets/_MAIN/Scripts/Core/Characters/Character.cs
+++ Assets/_MAIN/Scripts/Core/Characters/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DIALOGUE;
 using UnityEngine;
 
 namespace CHARACTERS
@@ -11,12 +12,16 @@ namespace CHARACTERS
     public abstract class Character
     {
         public string name = "";
+        public string displayName = "";
         public RectTransform root = null;
+        
+        public DialogueSystem dialogueSystem => DialogueSystem.instance;
 
         public Character(string name)
         {
             //base constructor
             this.name = name;
+            displayName = name;
         }
 
         //supported character types
@@ -28,5 +33,40 @@ namespace CHARACTERS
             Live2D,
             Model3D
         }
+
+
+        /*******************************************************
+         * Say lines independent from dialogue system and files
+         ********************************************************/
+        public Coroutine Say(string dialogue) => Say(new List<string> { dialogue });
+
+        public Coroutine Say(List<string> dialogue)
+        {
+            dialogueSystem.ShowSpeakerName(displayName);
+            dialogue = FormatDialogue(dialogue);
+            return dialogueSystem.Say(dialogue);
+        }
+
+        private List<string> FormatDialogue(List<string> dialogue)
+
+        {
+            List<string> formattedDialogue = new List<string>();
+
+            foreach (string line in dialogue)
+
+            {
+
+                formattedDialogue.Add($"{displayName} \"{line}\"");
+
+            }
+
+            return formattedDialogue;
+        }
+
+        /*******************************************************
+         * End Say lines independent from dialogue system and files
+         ********************************************************/
+
+
     }
 }
--- Assets/_TESTING/Scripts/TestCharacters.cs
+++ Assets/_TESTING/Scripts/TestCharacters.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CHARACTERS;
+using DIALOGUE;
 
 namespace TESTING
 {
@@ -10,12 +11,41 @@ namespace TESTING
         // Start is called before the first frame update
         void Start()
         {
+            //Character Stella2 = CharacterManager.instance.CreateCharacter("Stella");
+
+            StartCoroutine(Test());
+        }
+
+
+        /*******************************************************
+        * Say lines independent from dialogue system and files
+        ********************************************************/
+        IEnumerator Test()
+        {
             Character Elen = CharacterManager.instance.CreateCharacter("Elen");
             Character Stella = CharacterManager.instance.CreateCharacter("Stella");
-            Character Stella2 = CharacterManager.instance.CreateCharacter("Stella");
             Character Adam = CharacterManager.instance.CreateCharacter("Adam");
 
+
+            List<string> lines = new List<string>()
+            {
+                "Hi",
+                "This is a line.",
+            };
+
+            //yield return DialogueSystem.instance.Say(lines);
+
+            yield return Elen.Say(lines);
+
+            yield return Stella.Say(lines);
+
+
+            Debug.Log("Done");
         }
+
+        /*******************************************************
+         * End Say lines independent from dialogue system and files
+         ********************************************************/
 
         // Update is called once per frame
         void Update()
