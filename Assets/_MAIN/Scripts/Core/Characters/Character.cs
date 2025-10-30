029242beb11577df48867142c906a11ed1f66c55
jamisousa <jamiledsousa@gmail.com>
2025-10-30 13:06:25 -0300

    feat: character movement

--- Assets/_MAIN/Scripts/Core/Characters/Character.cs
+++ Assets/_MAIN/Scripts/Core/Characters/Character.cs
@@ -23,9 +23,13 @@ namespace CHARACTERS
 
         //Coroutines
         protected Coroutine co_revealing, co_hiding;
+        protected Coroutine co_moving;
 
         public bool isRevealing => co_revealing != null;
         public bool isHiding => co_hiding != null;
+
+        public bool isMoving => co_moving != null;
+
         public virtual bool isVisible => false;
 
 
@@ -145,6 +149,81 @@ namespace CHARACTERS
         {
             Debug.Log("Show and hide cannot be called from a base character type.");
             yield return null;
+        }
+
+        //set character position for movement
+        public virtual void SetPosition(Vector2 position)
+        {
+            if(root == null)
+            {
+                return;
+            }
+
+            (Vector2 minAnchorTarget, Vector2 maxAnchorTarget) = ConvertUITargetPosition(position);
+
+            root.anchorMin = minAnchorTarget;
+            root.anchorMax = maxAnchorTarget;
+        }
+
+    
+        public virtual Coroutine MoveToPosition(Vector2 position, float speed = 2f, bool smooth = false)
+        {
+
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (isMoving)
+            {
+                manager.StopCoroutine(co_moving);
+            }
+
+            co_moving = manager.StartCoroutine(MovingToPosition(position, speed, smooth));
+
+            return co_moving;
+        }
+
+        private IEnumerator MovingToPosition(Vector2 position, float speed, bool smooth)
+        {
+            (Vector2 minAnchorTarget, Vector2 maxAnchorTarget) = ConvertUITargetPosition(position);
+            Vector2 padding = root.anchorMax - root.anchorMin;
+
+            while(root.anchorMin != minAnchorTarget || root.anchorMax != maxAnchorTarget)
+            {
+               root.anchorMin = smooth ? Vector2.Lerp(root.anchorMin, minAnchorTarget, speed * Time.deltaTime)
+                    : Vector2.MoveTowards(root.anchorMin, minAnchorTarget, speed * Time.deltaTime * 0.35f);
+
+
+                root.anchorMax = root.anchorMin + padding;
+
+                if (smooth && Vector2.Distance(root.anchorMin, minAnchorTarget) <= 0.001f)
+                {
+                    root.anchorMin = minAnchorTarget;
+                    root.anchorMax = maxAnchorTarget;
+                    break;
+                }
+
+                yield return null;
+            }
+
+            Debug.Log("Done moving");
+            co_moving = null;
+
+        }
+
+        //convert ui target position to relative character anchor targets
+        public (Vector2, Vector2) ConvertUITargetPosition(Vector2 position) { 
+
+            Vector2 padding = root.anchorMax - root.anchorMin;
+
+            float maxX = 1f - padding.x;
+            float maxY = 1f - padding.y;
+
+            Vector2 minAnchorTarget = new Vector2(maxX * position.x, maxY * position.y);
+            Vector2 maxAnchorTarget = minAnchorTarget + padding;
+
+            return (minAnchorTarget, maxAnchorTarget);
         }
 
     }
