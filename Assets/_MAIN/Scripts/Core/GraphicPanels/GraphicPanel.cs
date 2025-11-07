94f1b468fc17380a20525fd14cae9a1cb2afc784
jamisousa <jamiledsousa@gmail.com>
2025-11-07 18:51:36 -0300

    feat: finish graphic panel logic

--- Assets/_MAIN/Scripts/Core/GraphicPanels/GraphicPanel.cs
+++ Assets/_MAIN/Scripts/Core/GraphicPanels/GraphicPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //contains layers that can be assigned images and videos on the UI
+
 [System.Serializable]
 public class GraphicPanel
 {
@@ -15,9 +16,7 @@ public class GraphicPanel
         for (int i = 0; i < layers.Count; i++)
         {
             if (layers[i].layerDepth == layerDepth)
-            {
                 return layers[i];
-            }
         }
 
         if (createIfDoesNotExist)
@@ -33,11 +32,9 @@ public class GraphicPanel
         GraphicLayer layer = new GraphicLayer();
         GameObject panel = new GameObject(string.Format(GraphicLayer.LAYER_OBJECT_NAME_FORMAT, layerDepth));
         RectTransform rect = panel.AddComponent<RectTransform>();
-
         panel.AddComponent<CanvasGroup>();
         panel.transform.SetParent(rootPanel.transform, false);
 
-        //set anchors to scale to the size of its parent panel // whole scale of  UI
         rect.anchorMin = Vector2.zero;
         rect.anchorMax = Vector2.one;
         rect.offsetMin = Vector2.zero;
@@ -46,34 +43,21 @@ public class GraphicPanel
         layer.panel = panel.transform;
         layer.layerDepth = layerDepth;
 
-        //check if there any layers that are greater layer depth than current one
         int index = layers.FindIndex(l => l.layerDepth > layerDepth);
-
-        //no match
-        if(index == -1)
-        {
+        if (index == -1)
             layers.Add(layer);
-        }
-        //matches
         else
-        {
             layers.Insert(index, layer);
-        }
 
-        //sort objects in hierarchy
         for (int i = 0; i < layers.Count; i++)
-        {
             layers[i].panel.SetSiblingIndex(layers[i].layerDepth);
-        }
 
         return layer;
     }
 
-    public void Clear()
+    public void Clear(float transitionSpeed = 1, Texture blendTexture = null, bool immediate = false)
     {
-        foreach(var layer in layers)
-        {
-            layer.Clear();
-        }
+        foreach (var layer in layers)
+            layer.Clear(transitionSpeed, blendTexture, immediate);
     }
 }
