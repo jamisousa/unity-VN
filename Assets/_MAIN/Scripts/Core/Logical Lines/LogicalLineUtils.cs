c0441f4fcf97746731e58d0820aa161275965bd0
jamisousa <jamiledsousa@gmail.com>
2025-11-14 13:50:00 -0300

    feat: operator lines handling

---
+++ Assets/_MAIN/Scripts/Core/Logical Lines/LogicalLineUtils.cs
@@ -0,0 +1,216 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DIALOGUE.LogicalLines
+{
+
+    //provides shared variable and method access to logical line classes
+    public static class LogicalLineUtils
+    {
+        public static class Encapsulation
+        {
+
+            public struct EncapsulatedData
+            {
+                public List<string> lines;
+                public int endingIndex;
+                public int startingIndex;
+            }
+
+            private const char ENCAPSULATION_START = '{';
+            private const char ENCAPSULATION_END = '}';
+
+
+            public static EncapsulatedData RipEncapsulationData(Conversation conversation, int startingIndex, bool ripHeaderAndEncapsulators = false)
+            {
+                int encapsulationDepth = 0;
+                EncapsulatedData data = new EncapsulatedData { lines = new List<string>(), startingIndex = startingIndex, endingIndex = 0 };
+
+                for (int i = startingIndex; i < conversation.Count; i++)
+                {
+                    string line = conversation.GetLines()[i];
+
+                    if (ripHeaderAndEncapsulators || (encapsulationDepth > 0 && !IsEncapsulationEnd(line)))
+                    {
+                        data.lines.Add(line);
+                    }
+
+                    if (IsEncapsulationStart(line))
+                    {
+                        encapsulationDepth++;
+                        continue;
+                    }
+
+                    if (IsEncapsulationEnd(line))
+                    {
+                        encapsulationDepth--;
+
+                        if (encapsulationDepth == 0)
+                        {
+                            data.endingIndex = i;
+                            break;
+                        }
+                    }
+                }
+
+                return data;
+            }
+
+            public static bool IsEncapsulationStart(string line) => line.Trim().StartsWith(ENCAPSULATION_START);
+            public static bool IsEncapsulationEnd(string line) => line.Trim().StartsWith(ENCAPSULATION_END);
+        }
+
+        public static class Expressions
+        {
+            public static HashSet<string> OPERATORS = new HashSet<string>() { "-", "-=", "+", "+=", "*", "*=", "/", "/=", "=" };
+            public static readonly string REGEX_ARITHMATIC = @"([-+*/=]=?)";
+            public static readonly string REGEX_OPERATOR_LINE = @"^\$\w+\s*(=|\+=|-=|\*=|/=|)\s*";
+
+            public static object CalculateValue(string[] expressionParts)
+            {
+                List<string> operandStrings = new List<string>();
+                List<string> operatorStrings = new List<string>();
+                List<object> operands = new List<object>();
+
+                for (int i = 0; i < expressionParts.Length; i++)
+                {
+                    string part = expressionParts[i].Trim();
+
+                    if (part == string.Empty)
+                        continue;
+
+                    if (OPERATORS.Contains(part))
+                        operatorStrings.Add(part);
+                    else
+                        operandStrings.Add(part);
+                }
+
+                foreach (string operandString in operandStrings)
+                {
+                    operands.Add(ExtractValue(operandString));
+                }
+
+                CalculateValue_DivisionAndMultiplication(operatorStrings, operands);
+
+                CalculateValue_AdditionAndSubtraction(operatorStrings, operands);
+
+                return operands[0];
+            }
+
+            private static void CalculateValue_DivisionAndMultiplication(List<string> operatorStrings, List<object> operands)
+            {
+                for (int i = 0; i < operatorStrings.Count; i++) {
+                    string operatorString = operatorStrings[i];
+
+                    if (operatorString == "*" || operatorString == "/")
+                    {
+                       double leftOperand = Convert.ToDouble(operands[i]);
+                        double rightOperand = Convert.ToDouble((operands[i + 1]));
+
+                        if(operatorString == "*")
+                        {
+                            operands[i] = leftOperand * rightOperand;
+                        }
+                        else
+                        {
+                            if(rightOperand == 0)
+                            {
+                                Debug.LogError("Cannot divide by zero.");
+                                return;
+                            }
+
+                            operands[i] = leftOperand / rightOperand;
+                        }
+                    }
+
+                    operands.RemoveAt(i + 1);
+                    operatorStrings.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            private static void CalculateValue_AdditionAndSubtraction(List<string> operatorStrings, List<object> operands)
+            {
+                for (int i = 0; i < operatorStrings.Count; i++)
+                {
+                    string operatorString = operatorStrings[i];
+
+                    if (operatorString == "+" || operatorString == "-")
+                    {
+                        double leftOperand = Convert.ToDouble(operands[i]);
+                        double rightOperand = Convert.ToDouble((operands[i + 1]));
+
+                        if (operatorString == "+")
+                        {
+                            operands[i] = leftOperand + rightOperand;
+                        }
+                        else
+                        {
+                            operands[i] = rightOperand - leftOperand;
+                        }
+
+                        operands.RemoveAt(i + 1);
+                        operatorStrings.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+
+            private static object ExtractValue(string value)
+            {
+                bool negate = false;
+
+                if (value.StartsWith('!'))
+                {
+                    negate = true;
+                    value = value.Substring(1);
+                }
+
+                if (value.StartsWith(VariableStore.VARIABLE_ID))
+                {
+                    string variableName = value.TrimStart(VariableStore.VARIABLE_ID);
+
+                    if (!VariableStore.HasVariable(variableName)) {
+
+                        Debug.Log($"Variable {variableName} does not exist.");
+                    }
+
+                    VariableStore.TryGetValue(variableName, out object val);
+
+                    if(val is bool boolVal && negate)
+                    {
+                        return !boolVal;
+                    }
+
+                    return val;
+                }
+
+                else if (value.StartsWith('\"') && value.EndsWith('\"'))
+                {
+
+                    value = TagManager.Inject(value, injectTags: true, injectVariables: true);
+                    return value.Trim('"');
+                }
+                else
+                {
+                    if (int.TryParse(value, out int intValue))
+                    {
+                        return intValue;
+                    }
+                    else if (float.TryParse(value, out float floatValue))
+                    {
+                        return floatValue;
+                    }
+                    else if (bool.TryParse(value, out bool boolValue))
+                    {
+                        return negate ? !boolValue : boolValue;
+                    }
+                    else return value;
+                }
+            }
+        }
+    }
+}
--- Assets/_MAIN/Scripts/Core/Logical Lines/Types/LL_Choice.cs
+++ Assets/_MAIN/Scripts/Core/Logical Lines/Types/LL_Choice.cs
@@ -4,22 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+using static DIALOGUE.LogicalLines.LogicalLineUtils.Encapsulation;
+
 namespace DIALOGUE.LogicalLines
 {
     //controls what happens when a choice is encountered in dialogue files
     public class LL_Choice : ILogicalLine
     {
         public string keyword => "choice";
-        private const char ENCAPSULATION_START = '{';
-        private const char ENCAPSULATION_END = '}';
+
         private const char CHOICE_IDENTIFIER = '-';
-        private bool IsEncapsulationStart(string line) => line.Trim().StartsWith(ENCAPSULATION_START);
-        private bool IsEncapsulationEnd(string line) => line.Trim().StartsWith(ENCAPSULATION_END);
+
         private bool IsChoiceStart(string line) => line.Trim().StartsWith(CHOICE_IDENTIFIER);
 
         public IEnumerator Execute(DIALOGUE_LINES line)
         {
-            RawChoiceData data = RipChoiceData();
+            var currentConversation = DialogueSystem.instance.conversationManager.conversation;
+            var progress = DialogueSystem.instance.conversationManager.conversationProgress;
+            EncapsulatedData data = RipEncapsulationData(currentConversation, progress, ripHeaderAndEncapsulators: true);
 
             List<Choice> choices = GetChoicesFromData(data);
 
@@ -51,39 +53,8 @@ namespace DIALOGUE.LogicalLines
             return (line.hasSpeaker && line.speakerData.name.ToLower() == keyword);
         }
 
-        private RawChoiceData RipChoiceData()
-        {
-            Conversation currentConversation = DialogueSystem.instance.conversationManager.conversation;
-            int currentProgress = DialogueSystem.instance.conversationManager.conversationProgress;
-            int encapsulationDepth = 0;
-            RawChoiceData data = new RawChoiceData { lines = new List<string>(), endingIndex = 0 };
 
-            for(int i = currentProgress; i < currentConversation.Count; i++)
-            {
-                string line = currentConversation.GetLines()[i];
-                data.lines.Add(line);
-
-                if (IsEncapsulationStart(line))
-                {
-                    encapsulationDepth++;
-                    continue;
-                }
-
-                if (IsEncapsulationEnd(line)) {
-                    encapsulationDepth--;
-
-                    if(encapsulationDepth == 0)
-                    {
-                        data.endingIndex = i;
-                        break;
-                    }
-                }
-            }
-
-            return data;
-        }
-
-        private List<Choice> GetChoicesFromData(RawChoiceData data)
+        private List<Choice> GetChoicesFromData(EncapsulatedData data)
         {
             List<Choice> choices = new List<Choice>();
             int encapsulationDepth = 0;
@@ -154,11 +125,6 @@ namespace DIALOGUE.LogicalLines
             choice.resultLines.Add(line);
         }
 
-        private struct RawChoiceData
-        {
-            public List<string> lines;
-            public int endingIndex;
-        }
 
         private struct Choice
         {
