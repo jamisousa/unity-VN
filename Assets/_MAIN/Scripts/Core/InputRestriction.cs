030504d370072c16f1a22f358d8292de5c5ca68d
jamisousa <jamiledsousa@gmail.com>
2025-11-29 19:18:03 -0300

    refactor: input panel layout and restrictions

---
+++ Assets/_MAIN/Scripts/Core/InputRestriction.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine;
+using System.Text.RegularExpressions;
+public class InputRestriction : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+
+    private Regex allowedChars = new Regex(@"^[\p{L}\s]+$");
+
+    private void Awake()
+    {
+        if (inputField == null)
+            inputField = GetComponent<TMP_InputField>();
+
+        inputField.onValueChanged.AddListener(ValidateText);
+    }
+
+    private void ValidateText(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+            return;
+
+        if (!allowedChars.IsMatch(text))
+        {
+            string filtered = FilterText(text);
+
+            inputField.onValueChanged.RemoveListener(ValidateText);
+            inputField.text = filtered;
+            inputField.onValueChanged.AddListener(ValidateText);
+        }
+    }
+
+    private string FilterText(string input)
+    {
+        var chars = input.ToCharArray();
+        string output = "";
+
+        foreach (char c in chars)
+        {
+            if (allowedChars.IsMatch(c.ToString()))
+                output += c;
+        }
+
+        return output;
+    }
+}
